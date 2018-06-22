1052a7a784bd424e08faf9a2db87f728c60b86db
Siddharth M <siddharth.m98@gmail.com>
2018-06-23 00:44:28 +0530

    Adding Activities

---
+++ app/src/main/java/com/example/siddharthm/instagramclone/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.example.siddharthm.instagramclone;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.siddharthm.instagramclone.Utils.BottomNavigationViewHelper;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setUpBottomNavigationView();
+    }
+    private void setUpBottomNavigationView(){
+        com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx bottomnavigationviewex = (com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx)findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setUpBottomNavigationView(bottomnavigationviewex);
+
+    }
+}
