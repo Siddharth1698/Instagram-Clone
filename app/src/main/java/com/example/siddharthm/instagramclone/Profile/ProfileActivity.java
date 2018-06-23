ca8266f751548cf652ee60cda140464b918e621c
Siddharth M <siddharth.m98@gmail.com>
2018-06-23 23:07:17 +0530

    Setting Profile Activity ToolBar

--- app/src/main/java/com/example/siddharthm/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/siddharthm/instagramclone/Profile/ProfileActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 import com.example.siddharthm.instagramclone.R;
 import com.example.siddharthm.instagramclone.Utils.BottomNavigationViewHelper;
@@ -16,8 +18,25 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        setUpBottomNavigationView();
+        setContentView(R.layout.activity_profile);
+//        setUpBottomNavigationView();
+        setUpToolBar();
+    }
+
+    private void setUpToolBar(){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()){
+                    case  R.id.profileMenu:
+                        break;
+                }
+                return false;
+            }
+        });
     }
     private void setUpBottomNavigationView(){
         com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx bottomnavigationviewex = (com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx)findViewById(R.id.bottomNavViewBar);
@@ -27,5 +46,11 @@ public class ProfileActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        return true;
     }
 }
