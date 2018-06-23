b15da394a03e8287ef79c4dafa54b73db846d26b
Siddharth M <siddharth.m98@gmail.com>
2018-06-24 04:04:48 +0530

    Account Settings Layout

--- app/src/main/java/com/example/siddharthm/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/siddharthm/instagramclone/Profile/ProfileActivity.java
@@ -1,12 +1,16 @@
 package com.example.siddharthm.instagramclone.Profile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 
 import com.example.siddharthm.instagramclone.R;
@@ -26,15 +30,13 @@ public class ProfileActivity extends AppCompatActivity {
     private void setUpToolBar(){
         Toolbar toolbar = (Toolbar)findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
 
-                switch (item.getItemId()){
-                    case  R.id.profileMenu:
-                        break;
-                }
-                return false;
+        ImageView profileMenu = (ImageView)findViewById(R.id.profileMenu);
+        profileMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext,AccountSettingActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -46,11 +48,5 @@ public class ProfileActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.profile_menu,menu);
-        return true;
     }
 }
