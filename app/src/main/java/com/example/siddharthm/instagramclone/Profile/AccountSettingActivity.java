3297fefa53d4088b5461212fadacddd836dad5dd
Siddharth M <siddharth.m98@gmail.com>
2018-06-24 14:47:01 +0530

    Account Setting Fragment Navigation

--- app/src/main/java/com/example/siddharthm/instagramclone/Profile/AccountSettingActivity.java
+++ app/src/main/java/com/example/siddharthm/instagramclone/Profile/AccountSettingActivity.java
@@ -3,19 +3,26 @@ package com.example.siddharthm.instagramclone.Profile;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.example.siddharthm.instagramclone.R;
+import com.example.siddharthm.instagramclone.Utils.SectionsStatePagerAdapter;
 
 import java.util.ArrayList;
 
 public class AccountSettingActivity extends AppCompatActivity {
 
     private Context mContext;
+    private SectionsStatePagerAdapter pagerAdapter;
+    private ViewPager mViewPager;
+    private RelativeLayout mRelativeLaoyout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +30,9 @@ public class AccountSettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account_settings);
         mContext = AccountSettingActivity.this;
         setupSettingList();
+        setupFragments();
+        mViewPager = (ViewPager)findViewById(R.id.container);
+        mRelativeLaoyout = (RelativeLayout)findViewById(R.id.relLayout1);
         ImageView backArrow = (ImageView)findViewById(R.id.backArrow);
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,15 +42,36 @@ public class AccountSettingActivity extends AppCompatActivity {
             }
         });
      }
+     private void setViewPager(int fragmentNumber){
+        mRelativeLaoyout.setVisibility(View.GONE);
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setCurrentItem(fragmentNumber);
+     }
 
+
+     private void setupFragments(){
+        pagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new EditProfileFragment(),getString(R.string.edit_profile_fragment));
+         pagerAdapter.addFragment(new SignOutFragment(),getString(R.string.sign_out_fragment));
+
+
+
+     }
      private void setupSettingList(){
          ListView listView = (ListView)findViewById(R.id.lvAccountSettings);
 
          ArrayList<String> options = new ArrayList<>();
-         options.add(getString(R.string.edit_profile));
-         options.add(getString(R.string.sign_out));
+         options.add(getString(R.string.edit_profile_fragment));
+         options.add(getString(R.string.sign_out_fragment));
 
          ArrayAdapter adapter = new ArrayAdapter(mContext,android.R.layout.simple_list_item_1,options);
          listView.setAdapter(adapter);
+
+         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 setViewPager(position);
+             }
+         });
      }
 }
--- app/src/main/java/com/example/siddharthm/instagramclone/Utils/SectionsStatePagerAdapter.java
+++ app/src/main/java/com/example/siddharthm/instagramclone/Utils/SectionsStatePagerAdapter.java
@@ -28,11 +28,38 @@ public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
         return mFragmentList.size();
     }
 
-    private void addFragment(Fragment fragment, String fragmentName){
+    public void addFragment(Fragment fragment, String fragmentName){
         mFragmentList.add(fragment);
         mFragment.put(fragment,mFragmentList.size()-1);
-        mFragmentNumbers.put(fragmentName,mFragmentList.size()-1).;
+        mFragmentNumbers.put(fragmentName,mFragmentList.size()-1);
         mFragmentNames.put(mFragmentList.size()-1,fragmentName);
+
+    }
+
+    public Integer getFragmentNumber(String fragmentName ){
+        if(mFragmentNumbers.containsKey(fragmentName)){
+            return mFragmentNumbers.get(fragmentName);
+        }else{
+            return null;
+        }
+
+    }
+
+    public Integer getFragmentNumber(Fragment fragment ){
+        if(mFragmentNumbers.containsKey(fragment)){
+            return mFragmentNumbers.get(fragment);
+        }else{
+            return null;
+        }
+
+    }
+
+    public String getFragmentName(Integer fragmentNumber ){
+        if(mFragmentNames.containsKey(fragmentNumber)){
+            return mFragmentNames.get(fragmentNumber);
+        }else{
+            return null;
+        }
 
     }
 }
