5c963d6894f51362de3dfb8e25fabbe450302cd1
Siddharth M <siddharth.m98@gmail.com>
2018-06-27 03:56:55 +0530

    Firebase Auth

--- app/src/main/java/com/example/siddharthm/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/example/siddharthm/instagramclone/Home/HomeActivity.java
@@ -1,28 +1,39 @@
 package com.example.siddharthm.instagramclone.Home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import com.example.siddharthm.instagramclone.Login.LoginActivity;
 import com.example.siddharthm.instagramclone.R;
 import com.example.siddharthm.instagramclone.Utils.BottomNavigationViewHelper;
 import com.example.siddharthm.instagramclone.Utils.SectionsPagerAdapter;
 import com.example.siddharthm.instagramclone.Utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class HomeActivity extends AppCompatActivity {
     private Context mContext = HomeActivity.this;
     private static int ACTIVITY_NUM = 0;
+    private static final String TAG = "HomeActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupFirebaseAuth();
         initImageLoader();
         setUpBottomNavigationView();
         setupViewPager();
@@ -55,4 +66,56 @@ public class HomeActivity extends AppCompatActivity {
         UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
         ImageLoader.getInstance().init(universalImageLoader.getConfig());
     }
+
+
+    private void checkCurrentUser(FirebaseUser user){
+        Log.d(TAG, "checkCurrentUser: checking if user is logged in.");
+
+        if(user == null){
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                //check if the user is logged in
+                checkCurrentUser(user);
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
--- app/src/main/java/com/example/siddharthm/instagramclone/Login/LoginActivity.java
+++ app/src/main/java/com/example/siddharthm/instagramclone/Login/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.example.siddharthm.instagramclone.Login;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.siddharthm.instagramclone.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Context mContext;
+    private ProgressBar mProgressBar;
+    private EditText mEmail,mPassword;
+    private TextView mPleaseWait;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+        mPleaseWait = (TextView)findViewById(R.id.pleasewait);
+
+        mEmail = (EditText)findViewById(R.id.input_email);
+        mPassword = (EditText)findViewById(R.id.input_password);
+        mContext = LoginActivity.this;
+        mProgressBar.setVisibility(View.GONE);
+        mPleaseWait.setVisibility(View.GONE);
     }
 }
